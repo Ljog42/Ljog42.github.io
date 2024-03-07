e4ca507f725786d197710f6b858f35a12d722cc1
LouisCoutel <louiscoutel75@gmail.com>
2024-03-07 11:32:28 +0100

    Fix: change resume folder and import statement

--- src/components/about/ResumeLink.vue
+++ src/components/about/ResumeLink.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+import CVurl from "../../assets/CV_Louis_Coutel.pdf"
+</script>
 <template>
     <a class="about-a lg:text-xl 2xl:text-xl text-base underline transition-all w-fit font-medium hover:text-utOrange duration-100"
         :href="CVurl" target="_blank">MonCV.pdf</a>
